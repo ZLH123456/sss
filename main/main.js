393ca0a978493b48853ab18b9b7c013824370610
ZLH123456 <1261407236@qq.com>
2017-06-23 13:57:08 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 // Write your cade below:
-+module.exports = function main(a,b) {
- -  		 +  var result=a%b;
- -}; 		 +  return result;
- +};
+module.exports = function main(a,b) {
+   		  var result=a%b;
+ 		  return result;
+ };
